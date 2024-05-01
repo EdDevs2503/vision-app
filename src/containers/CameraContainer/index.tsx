52f22666ee5a46b19e3315839457427d2fa550fd
Edisson Mata <edissondevs2000@gmail.com>
2024-05-01 15:37:36 -0400

    CameraContainer in progress

---
+++ src/containers/CameraContainer/index.tsx
@@ -0,0 +1,119 @@
+import { SafeAreaView } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useRef, useState } from "react";
+import { Camera, PhotoFile, VideoFile } from "react-native-vision-camera";
+import { useRecoilValue } from "recoil";
+
+import isPreviewDisabledSelector from "../../atoms/CameraConfiguration/selectors/isPreviewDisabled";
+import isVideoSelector from "../../atoms/CameraStatus/selectors/isVideo";
+import CameraControls from "../../compositions/CameraControls";
+import CameraViewer from "../../compositions/CameraViewer";
+import PreviewModal from "../../compositions/PreviewModal";
+import useFlash from "../../hooks/useFlash";
+import useHasPermission from "../../hooks/useHasPermission";
+import useSaveToCameraRoll from "../../hooks/useSaveToCameraRoll";
+import useSetDefault from "../../hooks/useSetDefault";
+import useVideoStatus from "../../hooks/useVideoStatus";
+const CameraContainer: React.FC = () => {
+  const [isPrevOpen, setIsPrevOpen] = useState(false);
+  const [videoFile, setVideoFile] = useState<VideoFile | undefined>();
+  const [photoFile, setPhotoFile] = useState<PhotoFile | undefined>();
+  const cameraRef = useRef<Camera>(null);
+  const { navigate } = useNavigation();
+  const hasPermission = useHasPermission();
+  const isVideo = useRecoilValue(isVideoSelector);
+  const isPreviewDisabled = useRecoilValue(isPreviewDisabledSelector);
+  const setDefault = useSetDefault();
+  const { flash } = useFlash();
+  const { switchRecordingStatus } = useVideoStatus();
+  const switchPrevModal = () => setIsPrevOpen(!isPrevOpen);
+  const { saveVideo, savePhoto } = useSaveToCameraRoll();
+
+  useEffect(() => {
+    setDefault();
+  }, [setDefault]);
+
+  useEffect(() => {
+    if (!hasPermission) {
+      navigate("Permissions");
+    }
+  }, [hasPermission, navigate]);
+
+  const handleRemovedPrev = () => {
+    switchPrevModal();
+    if (isVideo) {
+      setVideoFile(undefined);
+    } else {
+      setPhotoFile(undefined);
+    }
+  };
+  const handleSavePrev = () => {
+    if (isVideo) {
+      if (videoFile) saveVideo(videoFile);
+    } else {
+      if (photoFile) savePhoto(photoFile);
+    }
+  };
+
+  const handleTakePhoto = async () => {
+    if (cameraRef.current !== null) {
+      const photo = await cameraRef.current.takePhoto({
+        flash: flash ? "on" : "off",
+      });
+      if (!isPreviewDisabled) {
+        setPhotoFile(photo);
+        switchPrevModal();
+      }
+    }
+  };
+  const handleStartVideo = async () => {
+    if (cameraRef.current !== null) {
+      switchRecordingStatus(true);
+      cameraRef.current.startRecording({
+        flash: flash ? "on" : "off",
+        onRecordingFinished: (video) => {
+          if (!isPreviewDisabled) {
+            setVideoFile(video);
+            setIsPrevOpen(true);
+          } else {
+            saveVideo(video);
+          }
+        },
+        onRecordingError: (error) => console.error(error),
+      });
+    }
+  };
+  const handleStopVideo = async () => {
+    if (cameraRef.current !== null) {
+      switchRecordingStatus(false);
+      await cameraRef.current.stopRecording();
+    }
+  };
+
+  const handleClosePrev = () => {
+    switchPrevModal();
+    setPhotoFile(undefined);
+    setVideoFile(undefined);
+  };
+
+  return (
+    <SafeAreaView h="100%" backgroundColor="black">
+      <CameraViewer ref={cameraRef} />
+      <CameraControls
+        onTakePhoto={handleTakePhoto}
+        onStartVideo={handleStartVideo}
+        onStopVideo={handleStopVideo}
+      />
+      <PreviewModal
+        onHide={handleClosePrev}
+        isOpen={isPrevOpen}
+        photoFile={photoFile}
+        videoFile={videoFile}
+        onRemoved={handleRemovedPrev}
+        onSaved={handleSavePrev}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default CameraContainer;
